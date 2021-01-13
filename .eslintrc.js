911b2977fe25cb8ba51e052a66805f5b362f3527
Katsushi OUGI <ougi@to-r.net>
2021-01-13 13:53:25 +0900

    add eslint

---
+++ .eslintrc.js
@@ -0,0 +1,62 @@
+module.exports = {
+  extends: [
+    "airbnb-typescript",
+    "airbnb/hooks",
+    "plugin:@typescript-eslint/recommended",
+    "plugin:jest/recommended",
+    "prettier",
+    "prettier/react",
+    "prettier/@typescript-eslint",
+    "plugin:prettier/recommended",
+  ],
+  plugins: ["react", "@typescript-eslint", "jest"],
+  env: {
+    browser: true,
+    es6: true,
+    jest: true,
+  },
+  globals: {
+    Atomics: "readonly",
+    SharedArrayBuffer: "readonly",
+  },
+  parser: "@typescript-eslint/parser",
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true,
+    },
+    ecmaVersion: 2018,
+    sourceType: "module",
+    project: "./tsconfig.json",
+  },
+
+  rules: {
+    "linebreak-style": "off",
+    "prettier/prettier": [
+      "error",
+      {
+        bracketSpacing: true,
+        printWidth: 80,
+        endOfLine: "auto",
+        semi: false,
+        singleQuote: false,
+        trailingComma: "all",
+        useTabs: false,
+      },
+    ],
+  },
+
+  settings: {
+    "import/parsers": {
+      "@typescript-eslint/parser": [".ts", ".tsx", ".js", ".jsx"],
+    },
+    "import/resolver": {
+      node: {
+        extensions: [".js", "jsx", ".ts", ".tsx"],
+        paths: ["."],
+      },
+    },
+    react: {
+      version: "detect",
+    },
+  },
+};
